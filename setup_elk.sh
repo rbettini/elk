72c39a2214df0500536baa9fba363589bef703e3
rbettini <renato_bettini@yahoo.com.br>
2021-03-07 17:25:38 -0300

    Update setup_elk.sh

--- setup_elk.sh
+++ setup_elk.sh
@@ -4,16 +4,19 @@ rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
 cp elk.repo /etc/yum.repos.d/
 
-yum install --enablerepo=elk elasticsearch logstash kibana -y
+yum install --enablerepo=elk elasticsearch logstash kibana filebeat -y
 
 cp elastic.yaml /etc/elasticsearch
 cp logstash.yml /etc/logstash
-cp kibana.yml /etc/logstash
+cp kibana.yml /etc/kibana
+cp filebeat.yml /etc/filebeat
 
 systemctl enable elasticsearch.service
-systemctl enable logstash.service
 systemctl enable kibana.service
+#systemctl enable logstash.service
+#systemctl enable filebeat.service
 
 systemctl start elasticsearch.service
-systemctl start logstash.service
 systemctl start kibana.service
+#systemctl start logstash.service
+#systemctl start filebeat.service
